539de03bf379c28a5e23c553e2c1615eb0f37df2
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-03-20 00:37:20 +0700

    feat: Move middleware to proxy and add some rules

---
+++ src/proxy.ts
@@ -0,0 +1,83 @@
+import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
+import type { NextRequest } from "next/server";
+
+const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+export async function proxy(req: NextRequest) {
+    const token = req.cookies.get("token")?.value;
+    const url = req.nextUrl;
+
+    if (url.pathname === "/" || url.pathname === "/daftar") {
+        if (!token) return;
+        try {
+            const { payload } = await jwtVerify(token, secret);
+            switch (payload.role) {
+                case "ADMIN":
+                    return NextResponse.redirect(new URL("/admin", req.url));
+                case "DOKTER":
+                    return NextResponse.redirect(new URL("/dokter", req.url));
+                case "LAYAR":
+                    return NextResponse.redirect(new URL("/layar", req.url));
+                default:
+                    return;
+            }
+        } catch {
+            return;
+        }
+    }
+    if (url.pathname.startsWith("/admin")) {
+        if (!token) {
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+        try {
+            const { payload } = await jwtVerify(token, secret);
+
+            if (payload.role !== "ADMIN") {
+                return NextResponse.redirect(new URL("/", req.url));
+            } else if (url.pathname == "/admin") {
+                return NextResponse.redirect(new URL("/admin/pendaftaran", req.url));
+            }
+        } catch {
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+    }
+    if (url.pathname.startsWith("/dokter")) {
+        if (!token) {
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+        try {
+            const { payload } = await jwtVerify(token, secret);
+
+            if (payload.role !== "DOKTER") {
+                return NextResponse.redirect(new URL("/", req.url));
+            }
+        } catch {
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+    }
+    if (url.pathname.startsWith("/layar")) {
+        if (!token) {
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+        try {
+            const { payload } = await jwtVerify(token, secret);
+
+            if (payload.role !== "LAYAR" && payload.role !== "ADMIN") {
+                return NextResponse.redirect(new URL("/", req.url));
+            }
+        } catch {
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+    }
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        "/", "/daftar",
+        "/admin", "/admin/:path*", 
+        "/dokter", "/dokter/:path*",
+        "/layar"
+    ],
+};
