a78d93602168e5107b55d34899b7893f05cdcc88
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-03-20 00:36:35 +0700

    feat: Update libs

--- src/lib/antrean.ts
+++ src/lib/antrean.ts
@@ -1,22 +1,22 @@
 export const getPrefix = (kodeRuangan: string) => {
-  switch (kodeRuangan) {
-    case "cluster1":
-      return "A";
-    case "cluster2":
-      return "B";
-    case "cluster3":
-      return "C";
-    case "cluster4":
-      return "D";
-    default:
-      return "X";
-  }
+    switch (kodeRuangan) {
+        case "cluster1":
+            return "R1";
+        case "cluster2":
+            return "R2";
+        case "cluster3":
+            return "R3";
+        case "cluster4":
+            return "R4";
+        default:
+            return "RX";
+    }
 };
 
 export const formatNomorDisplay = (
-  kodeRuangan: string,
-  nomorAntrean: number
+    kodeRuangan: string,
+    nomorAntrean: number,
 ) => {
-  const prefix = getPrefix(kodeRuangan);
-  return `${prefix}-${nomorAntrean.toString().padStart(3, "0")}`;
+    const prefix = getPrefix(kodeRuangan);
+    return `${prefix}-${nomorAntrean.toString().padStart(3, "0")}`;
 };
