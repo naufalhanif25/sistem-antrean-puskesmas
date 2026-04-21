ad4297de824962fcc33d8185943443f9349984d6
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-04-22 00:45:21 +0700

    feat: Update user data props

--- src/app/props/UserData.ts
+++ src/app/props/UserData.ts
@@ -2,16 +2,17 @@ export type QueueStatus = "Dipanggil" | "Menunggu" | "Sedang Diperiksa" | "Seles
 export type PasswordInput = "text" | "password";
 export type UserRole = "DOKTER" | "ADMIN" | "LAYAR";
 
+export interface QueueItem {
+    id: number;
+    queueNumber: string;
+    patientName: string;
+    status: QueueStatus;
+    cluster: number;
+};
+
 export interface UserData {
     cluster: number;
     name: string;
     nip: string;
     role: UserRole;
-};
-
-export const statusOrder: Record<QueueStatus, number> = {
-    "Sedang Diperiksa": 1,
-    "Dipanggil": 2,
-    "Menunggu": 3,
-    "Selesai": 4,
 };
